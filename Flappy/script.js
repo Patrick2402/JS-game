3b2cfef2c65d20c244757b56ba19f8080602c784
Patryk <92256790+Patrick2402@users.noreply.github.com>
2022-10-10 00:20:06 +0200

    Add files via upload

---
+++ Flappy/script.js
@@ -0,0 +1,51 @@
+
+let littleBox = document.getElementById("object");
+var lbStyle = window.getComputedStyle(littleBox);
+
+document.addEventListener('keydown',(event) =>{
+
+   
+    switch (event.key) {
+        case "ArrowRight":
+            OutofXPosition("left",4);
+            break;
+        case "ArrowLeft":
+            OutofXPosition("left",3);
+            break;
+        case "ArrowUp":
+            OutofXPosition("top",1);
+            break;
+        case "ArrowDown":
+            OutofXPosition("top",2);
+            break;
+        default:
+            break;
+    }
+
+   
+});
+function OutofXPosition(topek,num){
+    var topValue = lbStyle.getPropertyValue(topek).replace("px", "");
+    if((Number(topValue) >= 700 && num == 2) 
+    || (Number(topValue) >= 700 && num == 4)
+    || (Number(topValue) <= 0 && num == 1)
+    || (Number(topValue) <= 0 && num == 3)) {
+ 
+    }else{
+        switch (num){
+            case 1:
+                littleBox.style.top = (Number(topValue) - 20) + "px";
+            break;
+            case 2: 
+                littleBox.style.top = (Number(topValue) + 20) + "px";
+            break;
+            case 3:
+                littleBox.style.left = (Number(topValue) - 20) + "px";
+            break;
+            case 4:
+                littleBox.style.left = (Number(topValue) + 20) + "px";
+            break;
+        }
+    
+    }
+}
